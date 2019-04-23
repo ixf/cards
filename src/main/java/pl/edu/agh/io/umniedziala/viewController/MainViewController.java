6b9fe81e4189c0b8cf4b01f2442475139d909afa
JuliaZur <julia17044@gmail.com>
2019-04-23 16:24:55 +0200

    simple colorful gui

--- src/main/java/pl/edu/agh/io/umniedziala/viewController/MainViewController.java
+++ src/main/java/pl/edu/agh/io/umniedziala/viewController/MainViewController.java
@@ -4,11 +4,46 @@
 
 package pl.edu.agh.io.umniedziala.viewController;
 
-
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 public class MainViewController {
     private AppController appController;
+
+    @FXML
+    private Label activity;
+
+    @FXML
+    private Pane menu;
+
+    @FXML
+    private Pane border;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Label app_name;
+
+    @FXML
+    private Text date;
+
+    @FXML
+    private Button left_date;
+
+    @FXML
+    private Button right_date;
+
+    @FXML
+    private BarChart activity_chart;
+
+    @FXML
+    private Button app_add;
 
     @FXML
     public void initialize(){
