b60274af84aaf6fd50cc5a20c4eea5da6a41ef71
Tomasz Karkocha <karkochat@gmail.com>
2019-04-24 00:30:00 +0200

    getting now the application exact name

--- src/main/java/pl/edu/agh/io/umniedziala/activeApplicationMonitor/ActiveApplicationListener.java
+++ src/main/java/pl/edu/agh/io/umniedziala/activeApplicationMonitor/ActiveApplicationListener.java
@@ -1,8 +1,10 @@
 package pl.edu.agh.io.umniedziala.activeApplicationMonitor;
 
-import com.sun.jna.Native;
+import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.ptr.IntByReference;
 
 public class ActiveApplicationListener extends Thread {
     private final int MAX_TITLE_LENGTH = 1024;
@@ -18,7 +20,7 @@ public class ActiveApplicationListener extends Thread {
     public void run() {
         while (true) {
             String windowName = getCurrentActiveWindowName();
-            String appName = windowName.split("-")[windowName.split("-").length - 1].replaceFirst(" ", "");
+            String appName = windowName.split("\\\\")[windowName.split("\\\\").length - 1];
 
             // debuging ======
             System.out.println("Active window title: " + windowName);
@@ -43,6 +45,31 @@ public class ActiveApplicationListener extends Thread {
         WinDef.HWND hwnd = User32.INSTANCE.GetForegroundWindow();
         User32.INSTANCE.GetWindowText(hwnd, buffer, MAX_TITLE_LENGTH);
 
-        return Native.toString(buffer);
+        String fgImageName = getImageName(hwnd);
+        if (fgImageName == null) {
+            return "Application not found";
+        } else {
+            return fgImageName;
+        }
+    }
+
+    private String getImageName(WinDef.HWND hwnd) {
+        IntByReference processId = new IntByReference();
+        User32.INSTANCE.GetWindowThreadProcessId(hwnd, processId);
+
+        // Open the process to get permissions to the image name
+        WinNT.HANDLE processHandle = Kernel32.INSTANCE.OpenProcess(
+                Kernel32.PROCESS_QUERY_LIMITED_INFORMATION,
+                false,
+                processId.getValue()
+        );
+
+        char[] buffer = new char[4096];
+        IntByReference bufferSize = new IntByReference(buffer.length);
+        boolean success = Kernel32.INSTANCE.QueryFullProcessImageName(processHandle, 0, buffer, bufferSize);
+
+        Kernel32.INSTANCE.CloseHandle(processHandle);
+
+        return success ? new String(buffer, 0, bufferSize.getValue()) : null;
     }
 }
