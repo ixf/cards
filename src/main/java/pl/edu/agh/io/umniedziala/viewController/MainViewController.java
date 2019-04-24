3c7afec0f1c46bb3ceb887c737abd905db0b9ab6
JuliaZur <julia17044@gmail.com>
2019-04-25 00:42:37 +0200

    date button handlers, getting app to add's name/path etc

--- src/main/java/pl/edu/agh/io/umniedziala/viewController/MainViewController.java
+++ src/main/java/pl/edu/agh/io/umniedziala/viewController/MainViewController.java
@@ -4,16 +4,32 @@
 
 package pl.edu.agh.io.umniedziala.viewController;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class MainViewController {
     private AppController appController;
+    DateFormat dateFormat = new SimpleDateFormat("EEEE, dd.MM.yyyy");
+    final FileChooser fileChooser = new FileChooser();
+
 
     @FXML
     private Label activity;
@@ -34,10 +50,10 @@ public class MainViewController {
     private Text date;
 
     @FXML
-    private Button left_date;
+    private ImageView left_date;
 
     @FXML
-    private Button right_date;
+    private ImageView right_date;
 
     @FXML
     private BarChart activity_chart;
@@ -47,11 +63,41 @@ public class MainViewController {
 
     @FXML
     public void initialize(){
-
+        Date current_date = new Date();
+        date.setText(dateFormat.format(current_date));
     }
 
     public void setAppController(AppController appController) {
         this.appController = appController;
+    }
+
+    @FXML
+    private void handle_left_date(MouseEvent event) throws IOException, ParseException {
+        String date_text = date.getText();
+        Date curr_date = dateFormat.parse(date_text);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(curr_date);
+        cal.add(Calendar.DATE,-1);
+        date.setText(dateFormat.format(cal.getTime()));
+    }
+
+    @FXML
+    public void handle_right_date(MouseEvent event) throws IOException, ParseException {
+        String date_text = date.getText();
+        Date curr_date = dateFormat.parse(date_text);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(curr_date);
+        cal.add(Calendar.DATE,1);
+        date.setText(dateFormat.format(cal.getTime()));
+    }
+
+    @FXML
+    public void handle_app_add(ActionEvent event) throws IOException {
+        List<File> list =
+                fileChooser.showOpenMultipleDialog(new Stage());
+        for (File file : list) {
+            System.out.println(file.getName());
+        }
     }
 
 }
