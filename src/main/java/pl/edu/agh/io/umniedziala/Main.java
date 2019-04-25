ff0d13a9c961bb50162490bfd5cf37cd89b76aee
Tomasz Karkocha <karkochat@gmail.com>
2019-04-25 20:12:49 +0200

    added database files to gitignore

--- src/main/java/pl/edu/agh/io/umniedziala/Main.java
+++ src/main/java/pl/edu/agh/io/umniedziala/Main.java
@@ -3,7 +3,7 @@ package pl.edu.agh.io.umniedziala;
 import pl.edu.agh.io.umniedziala.activeApplicationMonitor.ActiveApplicationListener;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         new ActiveApplicationListener(5000).start();
     }
 }
