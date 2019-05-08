3d12834cde913e843b35119a196083c6854ff5f4
Tomasz Karkocha <karkochat@gmail.com>
2019-05-08 20:51:44 +0200

    added update on ComputerRunningPeriod

--- src/main/java/pl/edu/agh/io/umniedziala/model/ComputerRunningPeriodEntity.java
+++ src/main/java/pl/edu/agh/io/umniedziala/model/ComputerRunningPeriodEntity.java
@@ -38,6 +38,22 @@ public class ComputerRunningPeriodEntity {
         return ComputerRunningPeriodEntity.findById(id);
     }
 
+    public static void update(final int id, final String startTime, final String endTime) {
+        String updateSql = String.format(
+                "UPDATE %s SET %s = '%s', %s = '%s' WHERE %s = %d"
+                , TABLE_NAME
+                , Columns.START_TIME, startTime
+                , Columns.END_TIME, endTime
+                , Columns.ID, id
+        );
+
+        try {
+            QuerryExecutor.update(updateSql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static Optional<ComputerRunningPeriodEntity> findById(final int id) {
         String findByIdSql = String.format("SELECT * FROM %s WHERE %s = %s", TABLE_NAME, Columns.ID, id);
 
